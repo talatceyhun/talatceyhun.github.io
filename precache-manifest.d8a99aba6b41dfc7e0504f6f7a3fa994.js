ac0da38567ec8ecdfd1879fba2fd946c08bc6d9c
Talat Ceyhun Büyükbayram <talatbuyukbayram@gmail.com>
2020-12-13 21:28:50 +0300

    portfolio site
    my portfolio site

---
+++ precache-manifest.d8a99aba6b41dfc7e0504f6f7a3fa994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "974bf8cbbf0f30761a9e",
+    "url": "./static/css/main.5ee8f14b.chunk.css"
+  },
+  {
+    "revision": "974bf8cbbf0f30761a9e",
+    "url": "./static/js/main.974bf8cb.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "./static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b6a63347da81f92e048a86936c755a2f",
+    "url": "./index.html"
+  }
+];
